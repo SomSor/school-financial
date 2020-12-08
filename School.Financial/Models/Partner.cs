393334e1e16a29b873f8435c9d0d00e6c0753279
Somkiet <somsor@perfenterprise.com>
2020-12-08 23:23:03 +0700

    update Partner view

--- School.Financial/Models/Partner.cs
+++ School.Financial/Models/Partner.cs
@@ -1,11 +1,25 @@
-﻿namespace School.Financial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School.Financial.Models
 {
     public class Partner : DbModelBase
     {
+        [Display(Name = "ชื่อ")]
         public string Name { get; set; }
+        [Display(Name = "เลขประจำตัวประชาชน/ผู้เสียภาษี")]
         public string VatNumber { get; set; }
+        [Display(Name = "ที่อยู่")]
         public string Address { get; set; }
+        [Display(Name = "ประเภท")]
         public string PartnerType { get; set; }
+
+        public string PartnerTypeString => PartnerType switch
+        {
+            Models.PartnerType.Normal => "ไม่ระบุ",
+            Models.PartnerType.Person => "นิติบุคคล",
+            Models.PartnerType.Shop => "ร้านค้า",
+            _ => string.Empty,
+        };
     }
 
     public class PartnerType
